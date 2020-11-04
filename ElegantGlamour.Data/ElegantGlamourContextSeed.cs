0a7598116123eff76694677e7061de2255f4e00b
ygout <yohann.goutaret@gmail.com>
2020-11-04 20:08:19 +0100

    modify startup for populate database

--- ElegantGlamour.Data/ElegantGlamourContextSeed.cs
+++ ElegantGlamour.Data/ElegantGlamourContextSeed.cs
@@ -52,6 +52,47 @@ namespace ElegantGlamour.Data
                 logger.LogError(ex.Message);
             }
         }
+        public static async Task SeedAsync(this ElegantGlamourDbContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                var rootPath = AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin"));
+
+                if (!context.PrestationCategories.Any())
+                {
+
+                    var categoriesData = File.ReadAllText("../ElegantGlamour.Data/SeedData/categories.json");
+
+                    var categories = JsonSerializer.Deserialize<List<PrestationCategory>>(categoriesData);
+
+                    foreach (var item in categories)
+                    {
+                        context.PrestationCategories.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Prestations.Any())
+                {
+                    var prestationsData = File.ReadAllText("../ElegantGlamour.Data/SeedData/prestations.json");
+
+                    var prestations = JsonSerializer.Deserialize<List<Prestation>>(prestationsData);
+
+                    foreach (var item in prestations)
+                    {
+                        context.Prestations.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<ElegantGlamourDbContext>();
+                logger.LogError(ex.Message);
+            }
+        }
 
     }
 }
